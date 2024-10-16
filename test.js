054b4c684e34aa45aee96dbc05f710aa801c450a
민재 백 <alstjwo98@kw.ac.kr>
2024-10-16 11:41:51 +0900

    33

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@jest/globals");
 const lib = require("./lib");
 test("avg([0, 15, 25]) should be 5", () => {
-expect(lib.avg([5, 10, 15])).toBe(5);
+expect(lib.avg([5, 10, 15])).toBe(10);
 });
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
