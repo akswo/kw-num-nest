176c232483fe36175a7c574aa7a25ed5aefb8e01
민재 백 <alstjwo98@kw.ac.kr>
2024-11-04 13:35:10 +0900

    12

--- main.js
+++ main.js
@@ -1,35 +1,56 @@
 #!/usr/bin/env node
 
-const lib = require("./lib"); // lib 모듈 임포트
+console.log(process.argv);
 
 let command = process.argv[2];
-let numbers = process.argv.slice(3).map((n) => parseFloat(n));
 
-// NaN 값 체크
-if (numbers.some((n) => isNaN(n))) {
-    console.log("Some arguments are not numbers!");
+let numbers = process.argv
+    .slice(3,process.argv.length)
+    .map((n)=>parseFloat(n));
+
+if(numbers.some((n)=>isNaN(n))){
+    console.log("Some arguments ar not numbers!");
     process.exit(1);
 }
 
-let result; // 결과 저장 변수
+function avg(numbers){
+    let s=0;
+    for(let i = 0;i<numbers.length;i++){
+        s+= numbers[i];
+    }
+    return s/numbers.length;
+}
 
-// 명령어에 따른 함수 호출
-switch (command) {
+function prime(num){
+    return false;
+}
+function factorial(num){
+    return -1;
+}
+exports.avg = avg;
+exports.prime = prime;
+exports.factorial = factorial;
+
+module.exports = {
+    avg,
+    prime,
+    factorial
+}
+
+let result;
+switch(command){
     case "avg":
-        result = lib.avg(numbers); // lib 모듈의 avg 함수 호출
+        result =avg(numbers);
         break;
     case "prime":
-        result = lib.prime(numbers[0]); // 첫 번째 숫자로 소수 여부 판단
+        result = prime(numbers[0]);
         break;
     case "factorial":
-        result = lib.factorial(numbers[0]); // 첫 번째 숫자로 팩토리얼 계산
+        result = factorial(numbers[0]);
         break;
     default:
         console.log("Wrong command!");
         process.exit(1);
 }
 
-// 결과 출력
-if (result !== undefined) {
-    console.log(result);
-}
+console.log(result);
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@jest/globals");
 const lib = require("./lib");
-test("avg([0, 15, 25]) should be 5", () => {
+test("avg([0, 15, 25]) should be 10", () => {
 expect(lib.avg([5, 10, 15])).toBe(10);
 });
 test("avg([-5, 5]) should be 0", () => {
