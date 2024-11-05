930173e6ac8b2d35387bcfad3705e8a303786c2a
민재 백 <alstjwo98@kw.ac.kr>
2024-11-05 18:38:59 +0900

    12

--- lib.test.js
+++ lib.test.js
@@ -4,7 +4,7 @@ test("avg([0, 10, 20]) should be 10", () => {
 expect(lib.avg([0, 10, 20])).toBe(10);
 });
 test("prime([17]) should be true", () => {
-    expect(lib.prime([17])).toBe(true);
+    expect(lib.prime([14])).toBe(true);
 });
 test("factorial(5) should be 120", ()=>{
     expect(lib.factorial([5])).toBe(120);
